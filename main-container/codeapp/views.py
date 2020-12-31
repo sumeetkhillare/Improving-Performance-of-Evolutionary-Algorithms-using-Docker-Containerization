9e74e32e554ccbb21804451683e4ce6079a74526
sanketpathak64 <sanketpathak64@gmail.com>
2020-12-31 19:00:47 +0530

    added evo algos

---
+++ main-container/codeapp/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+import requests
+from django.http import HttpResponse
+import json
+from .models import CodeInput,OptimizationCodeInput
+
+
+def home(request):
+    # c1=requests.get('http://python-container:8000')
+    # print(c1)
+    return render(request,'codeapp/home.html')
+def code(request):
+    return render(request,'codeapp/home.html')
+    # if request.method=="POST":
+    #     query=request.POST['array']
+    #     print(query)
+    #     code_inp=CodeInput()
+    #     code_inp.code_type='sorting'
+    #     code_inp.codeinput=query
+    #     code_inp.save()
+    #     c1=requests.get('http://insertion-sort:8000/check/?arr='+query).json() #Add ip address of your pc
+    #     c2=requests.get('http://quicksort-container:8000/check/?arr='+query).json()
+    #     c3=requests.get('http://bubble-sort:8000/check/?arr='+query).json() #Add ip address of your pc
+    #     c4=requests.get('http://selection-sort:8000/check/?arr='+query).json()
+    #     code_inp.delete()
+    #
+    # # return HttpResponse(str(c1['text'])+' : '+str(c1['arr'])+str(c2['text'])+' : '+str(c2['arr'])+"Quik sort "+str(c3['text'])+str(c3['arr'])+str(c4['text'])+' : '+str(c4['arr']))
+    # dic1={'sortname':str(c1['text']),'sortoutput':str(c1['arr'])}
+    # dic2={'sortname':str(c2['text']),'sortoutput':str(c2['arr'])}
+    # dic3={'sortname':str(c3['text']),'sortoutput':str(c3['arr'])}
+    # dic4={'sortname':str(c4['text']),'sortoutput':str(c4['arr'])}
+    # alldic=[dic1,dic2,dic3,dic4]
+    # alldic={'alldic':alldic}
+    # return render(request,'codeapp/output.html',alldic)
+def test(request):
+    code_input =list( CodeInput.objects.values('codeinput','code_type'))
+    print(code_input)
+    s=''
+    for i in code_input:
+        s+=str(i['codeinput'])+"    "+str(i['code_type'])+"    "
+    return HttpResponse(str(s))
+    # return HttpResponse('You are in container 2!!!')
+def optimizationcode(request):
+    if request.method=="POST":
+        opt_pop_size=request.POST['pop_size']
+        opt_gen=request.POST['gen']
+        opt_inp=OptimizationCodeInput()
+        opt_inp.code_type='optimization'
+        opt_inp.opt_pop_size=opt_pop_size
+        opt_inp.opt_gen=opt_gen
+        opt_inp.save()
+        jaya_container=requests.get('http://jaya-algo:8000/check/').json()
+        rao_container=requests.get('http://rao-algo:8000/check/').json()
+        rao2_container=requests.get('http://rao2-algo:8000/check/').json()
+        rao3_container=requests.get('http://rao3-algo:8000/check/').json()
+        opt_inp.delete()
+        dic_jaya={'algoname':str(jaya_container['text']),'algobest':str(jaya_container['best']),'algocoordi':str(jaya_container['algo-coordi'])}
+        dic_rao={'algoname':str(rao_container['text']),'algobest':str(rao_container['best']),'algocoordi':str(rao_container['algo-coordi'])}
+        dic_rao2={'algoname':str(rao2_container['text']),'algobest':str(rao2_container['best']),'algocoordi':str(rao2_container['algo-coordi'])}
+        dic_rao3={'algoname':str(rao3_container['text']),'algobest':str(rao3_container['best']),'algocoordi':str(rao3_container['algo-coordi'])}
+        alldic=[dic_jaya,dic_rao,dic_rao2,dic_rao3] #,dic_rao,dic_rao2
+        alldic={'alldic':alldic}
+        return render(request,'codeapp/output_optimization_containers.html',alldic)
---
+++ rao3-algo/codeapp/views.py
@@ -0,0 +1,133 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import psycopg2
+from django.http import HttpResponse
+
+
+
+
+# Python code of Rao-3 Algorithm
+# Unconstrained optimization
+# Sphere function
+
+import random
+import math
+import numpy as np
+def rao3Algo(Max_iter,SearchAgents_no):
+    maxfes = 500000 #Maximum functions evaluation
+    dim = 4 #Number of design variables
+    # SearchAgents_no = 10 #Population size
+    # Max_iter = math.floor(maxfes/SearchAgents_no) #Maximum number of iterations
+    lb = -20*np.ones(dim) #lower bound
+    ub = 20*np.ones(dim) #upper bound
+    def fitness(particle):
+        y = 0
+        for i in range(dim):
+            y= y*y - particle[i]**2 # sphere function
+        return y
+
+    Positions = np.zeros((SearchAgents_no, dim)) # search agent position
+    best_pos = np.zeros(dim) # search agent's best position
+    worst_pos = np.zeros(dim) # search agent's worst position
+
+    finval = np.zeros(Max_iter) # best score of each iteration
+    f1 = np.zeros(SearchAgents_no) # function value of current population
+    f2 = np.zeros(SearchAgents_no) # function value of updated population
+    for i in range(dim):
+        Positions[:, i] = np.random.uniform(0,1, SearchAgents_no) * (ub[i] - lb[i]) + lb[i]
+    for k in range(0,Max_iter):
+        best_score = float("inf")
+        worst_score = float("-inf")
+        for i in range(0,SearchAgents_no):
+
+            # Return back the search agents that go beyond the boundaries of the search space
+            for j in range(dim):
+                Positions[i,j]=np.clip(Positions[i,j], lb[j], ub[j])
+
+            f1[i]= fitness(Positions[i,:])
+            if f1[i] < best_score :
+                best_score=f1[i].copy(); # Update best
+                best_pos=Positions[i,:].copy()
+            if f1[i] > worst_score :
+                worst_score=f1[i].copy(); # Update worst
+                worst_pos=Positions[i,:].copy()
+            # Update the Position of search agents including omegas
+            finval[k] = best_score
+    #         print("The best solution is: ",best_score , " in iteration number: ",k+1)
+            Positioncopy = Positions.copy()
+            r = np.random.randint(SearchAgents_no, size=1)
+            for i in range(0,SearchAgents_no):
+                if (f1[i] < f1[r]):
+                    for j in range (0,dim):
+                        r1=random.random() # r1 is a random number in [0,1]
+                        r2=random.random() # r1 is a random number in [0,1]
+                        Positions[i,j]= Positions[i,j] + r1*(best_pos[j]-np.abs(worst_pos[j])) + r2*(np.abs(Positions[i,j])-Positions[i
+                        ,j])#change in position
+                        Positions[i,j]=np.clip(Positions[i,j], lb[j], ub[j])
+                else :
+                    for j in range (0,dim):
+                        r1=random.random() # r1 is a random number in [0,1]
+                        r2=random.random() # r1 is a random number in [0,1]
+                        Positions[i,j]= Positions[i,j] + r1*(best_pos[j]-np.abs(worst_pos[j])) + r2*(np.abs(Positions[r,j])-Positions[i,j]) #change in position
+                        Positions[i,j]=np.clip(Positions[i,j], lb[j], ub[j])
+                        f2[i] = fitness(Positions[i,:])
+
+            for i in range(0,SearchAgents_no):
+                if (f1[i] < f2[i]):
+                    Positions[i,:] = Positioncopy[i,:]
+    best_score = np.amin(finval)
+    return best_score,[best_pos[0],worst_pos[-1]]
+
+
+def home(request):
+    return render(request,'codeapp/home.html')
+def code(request):
+    if request.method=="POST":
+        query=request.POST['array']
+        # listofnum=query.split(',')
+        # lisofintnum=[]
+        returnstring='rao algo'
+        return HttpResponse('Sorted Array : '+returnstring)
+
+    return HttpResponse('Please give input')
+
+
+def check(request):
+    try:
+        connection = psycopg2.connect(user = "postgres",
+                                    password = "password",
+                                    host = "db",
+                                    port = "5432",
+                                    database = "postgres")
+        postgreSQL_select_Query = "SELECT opt_gen FROM codeapp_optimizationcodeinput where code_type='optimization';"
+        cursor = connection.cursor()
+        cursor.execute(postgreSQL_select_Query)
+        opt_gen=cursor.fetchone()
+        opt_gen=str(opt_gen)
+        opt_gen=opt_gen.replace("('","")
+        opt_gen=opt_gen.replace("',)","")
+        postgreSQL_select_Query2 = "SELECT opt_pop_size FROM codeapp_optimizationcodeinput where code_type='optimization';"
+        # cursor = connection.cursor()
+        cursor.execute(postgreSQL_select_Query2)
+        opt_pop_size=cursor.fetchone()
+        opt_pop_size=str(opt_pop_size)
+        opt_pop_size=opt_pop_size.replace("('","")
+        opt_pop_size=opt_pop_size.replace("',)","")
+        # Print PostgreSQL Connection properties
+        # x=str(main(int(opt_pop_size),int(opt_gen)))+str(' ')+str(opt_gen)+' '+str(opt_pop_size)
+        # returnstring=str(x)
+        x,y=rao3Algo(int(opt_pop_size),int(opt_gen))
+        print('rao algo container'+str(' ')+str(opt_gen)+' '+str(opt_pop_size))
+        return JsonResponse({'best':str(x),'algo-coordi':str(y),'text':'Rao3 Container'})
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while connecting to PostgreSQL", error)
+        return JsonResponse({'best':'Error','algo-coordi':'Error','text':'Rao3 Container'})
+    finally:
+        #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+    return JsonResponse({'best':'Error','algo-coordi':'Error','text':'Rao3 Container'})
+    # return HttpResponse('You are in container 1!!! + Result : '+str(arr))
